5de6c64214bb2793978c93cd8f1035c3ab71691d
detective0922 <detective0922@hotmail.com>
2016-05-22 10:46:21 +0800

    Refactor

--- Tetris/src/shape/IShape.java
+++ Tetris/src/shape/IShape.java
@@ -5,11 +5,16 @@ import node.TetrisNode;
 public class IShape extends TetrisShape {
 
 	@Override
-	protected void initShapeIndexs() {
+	protected int[][] initShapeIndexs() {
 		// TODO Auto-generated method stub
-		shapeIndex[1][0] = 1;
-		shapeIndex[1][1] = 1;
-		shapeIndex[1][2] = 1;
-		shapeIndex[1][3] = 1;
+		int[][] retArray = { 
+				{ 0, 0, 0, 0 }, 
+				{ 1, 1, 1, 1 }, 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0 } 
+				};
+
+		return retArray;
+			
 	}
 }
