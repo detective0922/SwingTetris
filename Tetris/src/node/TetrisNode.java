2f3c0563192cd298a2a52ffa50a66a905ff3bcbc
detective0922 <detective0922@hotmail.com>
2016-05-22 09:55:48 +0800

    refactor

--- Tetris/src/node/TetrisNode.java
+++ Tetris/src/node/TetrisNode.java
@@ -12,16 +12,18 @@ public class TetrisNode {
 	private Rectangle2D tetrisNodeRect;
 	private int SIDELENGTH;
 	
-	public TetrisNode(Graphics g, int x, int y, int size, Color color) {
+	public TetrisNode(/*Graphics g, int x, int y, */int size/*, Color color*/) {
 		SIDELENGTH = size;
-		tetrisNodeRect = new Rectangle2D.Double(x, y, size, size);
+		//tetrisNodeRect = new Rectangle2D.Double(x, y, size, size);
 	}
 	
-	public void draw(Graphics g, Color color){
+	public void draw(Graphics g, int x, int y, Color color){
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(color);
-		g2.draw(tetrisNodeRect);
-		g2.fill(tetrisNodeRect);
+		/*g2.draw(tetrisNodeRect);
+		g2.fill(tetrisNodeRect);*/
+		g2.drawRect(x, y, SIDELENGTH, SIDELENGTH);
+		g2.fillRect(x, y, SIDELENGTH, SIDELENGTH);
 	}
 	
 	public void TetrisNodeMoveByDirection(int DIR) {
