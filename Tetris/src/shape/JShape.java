cd881b36ae4e68839191e3a0b94264880973dc81
detective0922 <detective0922@hotmail.com>
2016-05-22 10:32:32 +0800

    refactor

--- Tetris/src/shape/JShape.java
+++ Tetris/src/shape/JShape.java
@@ -8,7 +8,7 @@ public class JShape extends TetrisShape {
 	protected void initShapeIndexs() {
 		// TODO Auto-generated method stub
 		shapeIndex[0][0] = 1;
-		shapeIndex[1][1] = 1;
+		shapeIndex[1][0] = 1;
 		shapeIndex[1][1] = 1;
 		shapeIndex[1][2] = 1;
 	}
