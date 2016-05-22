b4f5a2f2ab47d51426bd5c66047ceb041957ff62
detective0922 <detective0922@hotmail.com>
2016-05-22 11:31:22 +0800

    refactor

--- Tetris/src/tetrisMode/tetrisMode.java
+++ Tetris/src/tetrisMode/tetrisMode.java
@@ -1,5 +1,35 @@
 package tetrisMode;
 
+import shape.TetrisShape;
+
 public class tetrisMode {
+	
+	private int width;
+	private int height;
+	private TetrisShape[][] field;
+	
+	public tetrisMode(int width, int height) {
+		this.width = width;
+		this.height = height;
+		field = new TetrisShape[height][width];
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	public TetrisShape set(int row, int col, TetrisShape o) {
+		TetrisShape ret = field[row][col];
+		field[row][col] = o;
+		return ret;
+	}
+	
+	public TetrisShape get(int row, int col) {
+		return field[row][col];
+	}
 
 }
--- Tetris/src/tetrisView/tetrisView.java
+++ Tetris/src/tetrisView/tetrisView.java
@@ -1,5 +1,46 @@
 package tetrisView;
 
-public class tetrisView {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import shape.TetrisShape;
+import tetrisMode.tetrisMode;
+
+public class tetrisView extends JPanel{
+	
+	private static final int GRID_SIZE = 16;
+	private tetrisMode mode;
+	
+	public tetrisView(tetrisMode tetrisMode) {
+		this.mode = tetrisMode;
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(Color.GRAY);
+		for (int row = 0; row < mode.getHeight(); row++) {
+			g.drawLine(0, row * GRID_SIZE, mode.getWidth() * GRID_SIZE, row * GRID_SIZE);
+		}
+		for (int col = 0; col < mode.getWidth(); col++) {
+			g.drawLine(col * GRID_SIZE, 0, col * GRID_SIZE, mode.getHeight() * GRID_SIZE);
+		}
+		for (int row = 0; row < mode.getHeight(); row++) {
+			for (int col = 0; col < mode.getWidth(); col++) {
+				TetrisShape shape = mode.get(row, col);
+				if (shape != null) {
+					shape.draw(g, Color.BLACK);
+				}
+			}
+		}
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(mode.getWidth()*GRID_SIZE+1, mode.getHeight()*GRID_SIZE+1);
+	}
 
 }
