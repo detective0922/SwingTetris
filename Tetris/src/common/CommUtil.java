3809c2056fa86b502a9489ff2dd8aef48895bf9c
detective0922 <detective0922@hotmail.com>
2016-06-12 01:04:26 +0800

    testl

--- Tetris/src/common/CommUtil.java
+++ Tetris/src/common/CommUtil.java
@@ -1,5 +1,10 @@
 package common;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
+
 import node.TetrisNode;
 
 public class CommUtil {
@@ -8,11 +13,19 @@ public class CommUtil {
 		int yLen = intArray.length;
 		int xLen = intArray[0].length;
 		int[][] newArray = new int[xLen][yLen];
-		for (int i = 0; i < yLen; i++) {
-			for (int j = 0; j < xLen; j++) {
-				newArray[i][j]=intArray[xLen-j-1][i];  
+		Queue<Integer> queue = new LinkedList<Integer>();
+		for (int i = 0; i < xLen; i++) {
+			for (int j = 0; j < yLen; j++) {
+				//newArray[i][j]=intArray[xLen-j-1][i];  
+				queue.offer(intArray[j][i]);
 			}
 		}
+		for (int i = xLen - 1; i >= 0; i--) {
+			for (int j = 0; j < yLen; j++) {
+				newArray[i][j] = queue.poll();
+			}
+		}
+		
 		return newArray;
 
 	}
